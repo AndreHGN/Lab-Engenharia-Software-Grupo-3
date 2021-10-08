f50759a8b2917a1d413cde43d161209a16526f32
Gabriel Brandão <gabrielbc6@gmail.com>
2021-10-08 16:23:43 -0300

    Implemantado bd em mysql server

--- Projeto/apps/apps/settings.py
+++ Projeto/apps/apps/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'apps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+ 'default': {
+ 'ENGINE': 'django.db.backends.mysql',
+ 'NAME': 'Lote',
+ 'USER': 'admin',
+ 'PASSWORD': 'pass',
+ 'HOST': 'localhost',
+ 'PORT': '3306',
+ }
+ } 
+
 
 
 # Password validation
