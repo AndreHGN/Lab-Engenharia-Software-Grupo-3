2ee567d2772913bc87e53957d9f9cb6cd586237b
Gabriel Brandão <gabrielbc6@gmail.com>
2021-10-15 18:10:50 -0300

    Inicio da implementaçao do frontend

--- Projeto/apps/apps/urls.py
+++ Projeto/apps/apps/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('books_fbv/', include('books_fbv.urls')),
     path('books_fbv_user/', include('books_fbv_user.urls')),
     path('lote/', include('lote.urls')),
+    path('signup/', theme.views.signup_view, name='signup'),
+    path('profile/', theme.views.profile, name='profile'),
 
     # Enable built-in authentication views
     path('accounts/', include('django.contrib.auth.urls')),    
