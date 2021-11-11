8efda721ab0d4356cc9840f8bb6e851e59d87954
Gabriel Brandão <gabrielbc6@gmail.com>
2021-11-11 19:24:03 -0300

    adicionado lógicas de lance e modelo de pagamento

--- Projeto/apps/lote/urls.py
+++ Projeto/apps/lote/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
   path('lote/new/', views.lote_create, name='lote_new'),
   path('lote/edit/<int:pk>/', views.lote_update, name='lote_edit'),
   path('lote/delete/<int:pk>/', views.lote_delete, name='lote_delete'),
-  path('leilao/details/<slug:nomeLote>', views.leilao_details, name='leilao_details'),
+  path('leilao/details/<int:pk>', views.leilao_details, name='leilao_details'),
+  path('leilao/register/<int:pk>', views.create_leilao, name='create_leilao'),
+  path('lance/register/<int:pk>', views.create_lance, name='create_lance'),
 ]
